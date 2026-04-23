7ddfc9f88b77c18a7c052d0fdf85dea95e4f3364
Truc Nguyen <87359204+trucnguyen10@users.noreply.github.com>
2026-04-23 07:23:10 -0700

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,13 @@
+"""You'll implement this."""
+
+from typing import List
+
+from word_list import WordList
+
+
+class Solver:
+    def __init__(self, word_list: WordList) -> None:
+        self.word_list = word_list
+
+    def find_containers(self) -> List[str]:
+        pass
